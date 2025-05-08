0b25f583f37fa3a54c4b530b1eb13a0c8336889e
Sudechha Chakraborty <143380382+SudechhaChakraborty@users.noreply.github.com>
2025-05-08 19:08:42 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,4 @@
+# Get the latest Amazon Linux 2 AMI
 data "aws_ami" "amazon-linux" {
   most_recent = true
 
@@ -10,15 +11,40 @@ data "aws_ami" "amazon-linux" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = ["amazon"]  # Required to filter public AMIs
 }
 
+# Get the default subnet for us-east-1a
+data "aws_subnet" "default" {
+  default_for_az     = true
+  availability_zone  = "us-east-1a"
+}
+
+# Launch an EC2 instance
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+  ami           = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  subnet_id     = data.aws_subnet.default.id  # Needed to specify network
+  key_name      = "euran-jenkins"
 
   tags = {
     Environment = "dev"
-    Name = "${var.name}-server"
+    Name        = "${var.name}-server"
   }
 }
+
+# Variable definition
+variable "name" {
+  default = "jenkins"
+}
+
+# Store Terraform state in S3
+terraform {
+  backend "s3" {
+    bucket = "sude-terraform"
+    key    = "server_name/statefile"
+    region = "us-east-1"
+  }
+}
+
